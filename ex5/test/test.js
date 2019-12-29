67d4784d69f7c228b4ca2d2f8898485d0541c367
izifrida <ivanka_va@ukr.net>
2019-12-29 19:53:06 +0200

    Task 13 point 5

--- ex5/test/test.js
+++ ex5/test/test.js
@@ -5,11 +5,31 @@ var supertest = require("supertest");
 var server = supertest.agent("http://localhost:3000");
 
 // UNIT test begin
-describe('GET /', function() {
-      it('respond with html', function(done) {
-         server
-         .get('/')
-         .expect('Content-Type', /html/)
-         .expect(200, done);
+describe('GET /', function () {
+      it('respond with html', function (done) {
+            server
+                  .get('/')
+                  .expect('Content-Type', /html/)
+                  .expect(200, done);
+      });
+
+      it('respond with 1 + 2 = 3 for variables values specified in the source code', function (done) {
+            server
+                  .get('/')
+                  .expect('Content-Type', /html/)
+                  .expect(function (res) {
+                        res.text.includes('1 + 2 = 3');
+                  })
+                  .expect(200, done);
+      });
+
+      it('respond with 1 + 2 = 3 for variable values passed in the URL', function (done) {
+            server
+                  .get('/add/1/2')
+                  .expect('Content-Type', /html/)
+                  .expect(function (res) {
+                        res.text.includes('1 + 2 = 3');
+                  })
+                  .expect(200, done);
       });
 });
