b639e8090534f6bb0065a6e55585ace6e43bf499
izifrida <ivanka_va@ukr.net>
2020-01-14 16:44:36 +0200

    Server script

--- extra.1/server.js
+++ extra.1/server.js
@@ -3,16 +3,16 @@ var url = require("url");
 var fs = require("fs");
 var file = 'form.html';
 
-http.createServer(function (request, response) {
+http.createServer(function(request, response) {
     console.log("--------------------------------------");
     console.log("The relative URL of the current request: " + request.url + "\n");
     var url_parts = url.parse(request.url, true);  // parsing (relative) URL
 
     switch (url_parts.pathname) {
         case '/':
-            fs.stat(file, function (err, stats) {
+            fs.stat(file, function(err, stats) {
                 if (err == null) {
-                    fs.readFile(file, function (err, data) {
+                    fs.readFile(file, function(err, data) {
                         response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
                         response.write(data);
                         response.end();
@@ -27,15 +27,47 @@ http.createServer(function (request, response) {
             break;
 
         case '/file':
-            var fileName = url_parts.query['fileName'];
-            fs.stat(fileName, function (err, stats) {
-                if (err == null) {
-                    fs.readFile(fileName, function (err, data) {
-                        response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+            const fileInput = url_parts.query['file'];
+            console.log(fileInput);
+            var operation = url_parts.query['operation'];
 
-                        response.write(data);
-                        response.end();
-                    });
+            const fileName = fileInput.split(' ')[0];
+
+
+            fs.stat(fileName, function(err, stats) {
+                if (err == null) {
+                    if (operation === 'removeFile') {
+                        fs.unlink(fileName, function(err, data) {
+                            if (err) {
+                                response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+                                response.write('Error occured: ' + err);
+                                response.end();
+                                return;
+                            }
+
+                            response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+                            response.write('The ' + fileName + ' was removed');
+                            response.end();
+                        });
+                    } else {
+                        const newName = fileInput.split(' ')[1];
+
+                        fs.rename(fileName, newName, function(err, data) {
+                            if (err) {
+                                response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+                                response.write('Error occured: ' + err);
+                                response.end();
+                                return;
+                            }
+
+                            response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+
+                            response.write('The ' + fileName + ' was renamed to ' + newName);
+                            response.end();
+                        });
+                    }
+
+
                 }
                 else {
                     response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
